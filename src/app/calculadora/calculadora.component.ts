3b982fbba1abd910bbc0e0fe7193c4121195356a
hmm <henrique-monteiro@hotmail.com>
2025-10-08 18:08:36 -0300

    adiciona lógica para a calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.scss'
+  styleUrls: ['./calculadora.component.scss']
 })
 export class CalculadoraComponent {
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
 
+  somar(): void {
+    this.resultado = this.num1 + this.num2;
+  }
 }
