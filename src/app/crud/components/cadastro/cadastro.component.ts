748c63cc430b765b214985b85245f0e4f23694c2
hmm <henrique-monteiro@hotmail.com>
2026-03-30 11:01:21 -0300

    implementa cadastro de clientes

--- src/app/crud/components/cadastro/cadastro.component.ts
+++ src/app/crud/components/cadastro/cadastro.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { Cliente } from './Cliente';
+import { ClienteService } from '../../service/cliente.service';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule } from '@angular/forms'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [
+    FlexLayoutModule, 
+    MatCardModule, 
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  cliente: Cliente = Cliente.newCliente();
+
+  constructor(private service: ClienteService){
+
+  }
+
+  salvar(){
+    this.service.salvar(this.cliente);    
+  }
 
 }
