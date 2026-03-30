6d42bf0b996a9a07e9a5cb97cf84687dcf9ebc87
hmm <henrique-monteiro@hotmail.com>
2026-03-30 14:11:32 -0300

    implementa consulta e atualização de clientes

--- src/app/crud/components/cadastro/cadastro.component.ts
+++ src/app/crud/components/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from './Cliente';
 import { ClienteService } from '../../service/cliente.service';
 import { FlexLayoutModule } from '@angular/flex-layout'
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input'
 import { FormsModule } from '@angular/forms'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -24,15 +25,40 @@ import { MatButtonModule } from '@angular/material/button'
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit{
   cliente: Cliente = Cliente.newCliente();
+  atualizando: boolean = false;
 
-  constructor(private service: ClienteService){
+  constructor(
+    private service: ClienteService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){
 
   }
 
-  salvar(){
-    this.service.salvar(this.cliente);    
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe( (query: any) => {
+        const params = query['params']
+        const id = params['id']
+        if(id){
+          let clienteEncontrado = this.service.buscarClientePorId(id);
+          if(clienteEncontrado){
+            this.atualizando = true;
+            this.cliente = clienteEncontrado;
+          }
+        }
+    })
+  }
+
+  salvar() {
+    if(!this.atualizando){
+      this.service.salvar(this.cliente); 
+      this.cliente = Cliente.newCliente();   
+    } else {
+      this.service.atualizar(this.cliente);
+      this.router.navigate(['/consulta'])
+    }   
   }
 
 }
